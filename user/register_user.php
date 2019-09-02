943408cadedddfb5824e1ef28b5d838a14304f99
stepan2019 <developer.jj@outlook.com>
2019-09-03 07:04:30 +0900

    update

--- user/register_user.php
+++ user/register_user.php
@@ -24,7 +24,7 @@ if (isset($_POST['register'])) {
         global $config_live_site;
         // add activation code to db record
         $activation_code = generate_random();
-        $res_act = $db->query("update " . TABLE_USERS . " set activation='$activation_code' where `email` = '$email'");
+        $res_act = $db->query("update user set activation='$activation_code' where `email` = '$email'");
         $account = urlencode($_POST['email']);
         if (!$mail_setting['html_mails'])
             $act_link = $config_live_site . '/activate_account.php?account=' . $account . '&activation=' . $activation_code;
